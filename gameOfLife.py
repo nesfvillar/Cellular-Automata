f32ebbb477431ceb121fdc87d9ff928af2f080dc
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-08-09 18:38:41 -0300

    Implemented a game class

--- gameOfLife.py
+++ gameOfLife.py
@@ -2,13 +2,26 @@ from automata import *
 import tkinter
 
 
-def gameOfLife(cell):
-    if cell.state == "alive" and (
-        cell.countNeighbours("alive") < 2 or cell.countNeighbours("alive") > 3
-    ):
-        cell.newState = "dead"
-    elif cell.state == "dead" and cell.countNeighbours("alive") == 3:
-        cell.newState = "alive"
+class GameOfLife(GridAutomata):
+    def __init__(self, width, heigth, colours={"alive": "green", "dead": "red"}, defaultState="dead"):
+        super().__init__(width, heigth, defaultState)
+        self.colours = colours
+
+    def randomizeState(self):
+        return super().randomizeState(("alive", "dead"))
+
+    def update(self):
+        super().update(self.rules)
+
+    @staticmethod
+    def rules(cell):
+        if cell.state == "alive" and (
+            cell.countNeighbours("alive") < 2 or
+            cell.countNeighbours("alive") > 3
+        ):
+            cell.newState = "dead"
+        elif cell.state == "dead" and cell.countNeighbours("alive") == 3:
+            cell.newState = "alive"
 
 
 def main():
@@ -18,21 +31,22 @@ def main():
     root = tkinter.Tk()
     root.title("Conway's Game of Life")
 
-    canvas = tkinter.Canvas(root, bg="white", width=CANVAS_WIDTH, height=CANVAS_HEIGTH)
+    canvas = tkinter.Canvas(
+        root, bg="white", width=CANVAS_WIDTH, height=CANVAS_HEIGTH)
     canvas.pack()
 
-    automata = GridAutomata(AUTOMATA_WIDTH, AUTOMATA_HEIGTH)
-    automata.randomizeState(("alive", "dead"))
-    colors = {"alive": "green", "dead": "red"}
+    gameOfLife = GameOfLife(AUTOMATA_WIDTH, AUTOMATA_HEIGTH)
+    gameOfLife.randomizeState()
 
     def updateCanvas():
-        for cell in automata.cells:
+        for cell in gameOfLife.cells:
             x1 = cell.id[0] * CANVAS_WIDTH / AUTOMATA_WIDTH
             y1 = cell.id[1] * CANVAS_HEIGTH / AUTOMATA_HEIGTH
             x2 = (cell.id[0] + 1) * CANVAS_WIDTH / AUTOMATA_WIDTH
             y2 = (cell.id[1] + 1) * CANVAS_HEIGTH / AUTOMATA_HEIGTH
-            canvas.create_rectangle(x1, y1, x2, y2, fill=colors[cell.state])
-        automata.update(gameOfLife)
+            canvas.create_rectangle(
+                x1, y1, x2, y2, fill=gameOfLife.colours[cell.state])
+        gameOfLife.update()
         canvas.after(500, updateCanvas)
 
     updateCanvas()
